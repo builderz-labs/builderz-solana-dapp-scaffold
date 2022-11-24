44b3c9980bbf2832fd4616d123f8585b59551dcb
0xNyk <0xnykcd@googlemail.com>
2022-11-24 19:49:16 +0100

    init

---
+++ contexts/ContextProvider.tsx
@@ -0,0 +1,49 @@
+import { WalletAdapterNetwork, WalletError } from "@solana/wallet-adapter-base";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider as ReactUIWalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  SolletExtensionWalletAdapter,
+  SolletWalletAdapter,
+  TorusWalletAdapter,
+  // LedgerWalletAdapter,
+  // SlopeWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import { FC, ReactNode, useCallback, useMemo } from "react";
+
+export const WalletContextProvider: FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new SolletWalletAdapter({ network }),
+      new SolletExtensionWalletAdapter({ network }),
+      new TorusWalletAdapter(),
+      // new LedgerWalletAdapter(),
+      // new SlopeWalletAdapter(),
+    ],
+    [network]
+  );
+
+  const onError = useCallback((error: WalletError) => {
+    console.error(error);
+  }, []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} onError={onError} autoConnect={true}>
+        <ReactUIWalletModalProvider>{children}</ReactUIWalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,34 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { ThirdwebProvider } from "@thirdweb-dev/react/solana";
 import { Network } from "@thirdweb-dev/sdk/solana";
 import type { AppProps } from "next/app";
+import AppBar from "../components/AppBar";
 import "../styles/globals.css";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 // Change the network to the one you want to use: "mainnet-beta", "testnet", "devnet", "localhost" or your own RPC endpoint
 const network: Network = "mainnet-beta";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <ThemeProvider theme={darkTheme}>
+
     <ThirdwebProvider network={network}>
+      <CssBaseline />
       <WalletModalProvider>
+        <AppBar />
         <Component {...pageProps} />
       </WalletModalProvider>
     </ThirdwebProvider>
+    </ThemeProvider>
   );
 }
 
