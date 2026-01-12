f83da5378a9e987847c906651013e2a7adae925d
Nyk <0xnykcd@googlemail.com>
2026-01-12 22:07:35 +0700

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  turbopack: {
-    root: process.cwd(),
-  },
+  /* config options here */
 };
 
 export default nextConfig;
